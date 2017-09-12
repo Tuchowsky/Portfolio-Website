a18521397ec8a74847d63421d325c48ed344d0af
Tuchowsky <arthur.tuchowsky@gmail.com>
2017-09-12 22:00:13 +0200

    Making svg larger on hover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
         $('.animation').css({'width': '220%', 'height': '300%'});
     });
     $('.no-shape').mouseleave(function(){
-        $('.animation').css({'width': '100%', 'height': '100%'});
+        $('.animation').css({'width': '180%', 'height': '150%'});
         
     });
 });
