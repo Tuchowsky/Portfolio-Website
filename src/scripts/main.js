7dbc59b785865b3f8d53b3ba7f530eddefd0d1a5
Tuchowsky <arthur.tuchowsky@gmail.com>
2017-09-21 17:48:39 +0200

    New Animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
-    $('.no-shape').mouseover(function(){
-        $('.animation').css({'width': '220%', 'height': '300%'});
-    });
-    $('.no-shape').mouseleave(function(){
-        $('.animation').css({'width': '100%', 'height': '100%'});
+    // $('.no-shape').mouseover(function(){
+    //     $('.animation').css({'width': '220%', 'height': '300%'});
+    // });
+    // $('.no-shape').mouseleave(function(){
+    //     $('.animation').css({'width': '100%', 'height': '100%'});
         
     });
     //projects container
